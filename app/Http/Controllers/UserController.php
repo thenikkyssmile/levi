7c47c7bbf200d638f13fc60fab5f2dc6a2144adf
thenikkyssmile <66449852+thenikkyssmile@users.noreply.github.com>
2020-07-01 19:07:10 +0700

    second

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\User;
 
 class UserController extends Controller
 {
     public function index()
     {
+        //$data = [
+            //'name'      => 'elon',
+            //'email'     => 'elon@admin.com',
+            //'password'  => 'password',
+        //];
+        //User::create($data);
+        // --- Eloquent ORM ---
+        //$user = new User();
+        //$user->name = 'nikkys';
+        //$user->email = 'nikkys@nikkys.com';
+        //$user->password = bcrypt('nikkys');
+        //$user->save();
+
+        //User::where('id', 3)->delete();
+        //var_dump($user);
+
+        //User::where('id', 4)->update(['name'=>'admin', 'email'=>'nikkys@admin.com', 'password'=>bcrypt('password')]);
+
+        $user = User::all();
+
+        // --- Raw SQL Queries ---
+        //dd($user);
+
         // DB::insert('insert into users (name,email,password) values (?,?,?)', ['nikkys', 'nikkys@admin.com', 'pasword']);
        
        // DB::update('update users set name = ?, email = ? where id = 2', ['admin', 'admin@admin.com']);
@@ -16,7 +40,8 @@ class UserController extends Controller
        // DB::delete('delete from users where id =  1');
 
        //$users = DB::select('select * from users');
-       //return $users;
+
+       return $user;
        return view('home');
     }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    public function getNameAttribute($name)
+    {
+        return 'My name is: ' . ucfirst($name);
+    }
 }
